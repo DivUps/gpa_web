49373c79f3c3971ee0945b44845ecdebca438d73
Hana Jiffry <hanajiffryscg@gmail.com>
2022-04-14 13:28:14 +0530

    Finalized header

--- frontend/src/Components/General/Header/Header.js
+++ frontend/src/Components/General/Header/Header.js
@@ -7,24 +7,21 @@ class Header extends React.Component{
     render(){
         return(
          <>
-         <Navbar bg="light" variant="light" style={{padding:'25px'}} >
-             <Container fluid >
+         <Navbar bg="light" variant="light" style={{height:'100px'}} >
+             <Container fluid style={{position:'sticky-top'}}>
                 <Row>
-                    <Col xs lg="2">
+                    <Col xs lg="1" style={{paddingLeft:'90px'}}>
                     <NavbarBrand>
                      <img className='logo' src='../Assets/gpa.png' alt='logo'/>
                     </NavbarBrand>
                     </Col>
-                    <Col  xs lg="0.5">
-                    <NavbarBrand>GPA Calculator</NavbarBrand>
+                    <Col xs lg='2.5' style={{paddingRight:'200px',paddingLeft:'75px'}}>
+                    <NavbarBrand className='Heading' style={{color:'#1c4966'}}>GPA Calculator</NavbarBrand>
                     </Col>
-                    <Col  xs lg="2">
-                    <NavLink className='Hlink'>Home</NavLink>
-                    </Col>
-                    <Col  xs lg="6.5">
+                    <Col xs lg='1' style={{paddingLeft:'250px'}}>
                     <NavLink className='Hlink'>User</NavLink>
                     </Col>
-                    <Col >
+                    <Col xs lg='1' style={{paddingLeft:'90px'}}>
                     <NavLink className='Hlink'>Logout</NavLink>
                     </Col>
                 </Row>
